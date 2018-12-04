652a18cb1b6e58eaa6f001c53ba9c9837ea6b97d
Tomi Laurell <tomi.laurell@gmail.com>
2018-12-04 11:49:59 +0200

    CloudFront distribution id update

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #yarn build
 sls syncToS3
-aws --profile app cloudfront create-invalidation --distribution-id E3SX3YRVLR1RCI --paths "/*"
+aws --profile app cloudfront create-invalidation --distribution-id E37KF3YUBDHKK6 --paths "/*"
