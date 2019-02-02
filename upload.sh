527e6b1b35a65edbdf1319d407380b9555b0fe66
Tomi Laurell <tomi.laurell@gmail.com>
2019-02-02 17:48:31 +0200

    Direct s3 sync with aws tools

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #yarn build
-sls syncToS3
+aws --profile app s3 sync ./build/ s3://opensongbook.laurell.org
 aws --profile app cloudfront create-invalidation --distribution-id E37KF3YUBDHKK6 --paths "/*"
